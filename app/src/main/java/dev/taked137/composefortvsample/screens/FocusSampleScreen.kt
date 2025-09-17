d49f212368d6a2f1dc646989b137d5f4b911989c
taked137 <taked137@gmail.com>
2025-09-17 23:27:49 +0900

    add comments to FocusSampleScreen

--- app/src/main/java/dev/taked137/composefortvsample/screens/FocusSampleScreen.kt
+++ app/src/main/java/dev/taked137/composefortvsample/screens/FocusSampleScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusRestorer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.taked137.composefortvsample.components.DetailList
@@ -21,18 +23,35 @@ internal fun FocusSampleScreen(
   modifier: Modifier = Modifier,
 ) {
   val (focusedMasterListIndex, setIndex) = remember { mutableIntStateOf(0) }
+  val focusRequester = remember { FocusRequester() }
 
   Row(
     modifier = modifier,
   ) {
     MasterList(
-      modifier = Modifier.width(200.dp),
+      modifier = Modifier
+        .width(200.dp)
+        // ⭐️[focus] step1: Enable focusRestorer to remember and restore focus position
+        // .focusRestorer() // applied to Column, but verticalScroll is also applied internally, so focusGroup is not needed
+        ,
       onItemFocused = setIndex,
     )
-    DetailList(
-      modifier = Modifier.weight(1f),
-      listIndex = focusedMasterListIndex,
-    )
+    // ⭐️[focus] step2: Use `key` to reset focusRestorer state when the ui changes
+    // key(focusedMasterListIndex) {
+      DetailList(
+        modifier = Modifier
+          .weight(1f)
+          // ⭐️[focus] step1: Enable focusRestorer to remember and restore focus position
+          // .focusRestorer() // applied to LazyColumn, so focusGroup is not needed
+          // ⚠️Note: When using step3, remove step1's .focusRestorer() above
+
+          // ⭐️[focus] step3: Use FocusRequester as a fallback target for focusRestorer
+          // .focusRequester(focusRequester)
+          // .focusRestorer(focusRequester) // applied to LazyColumn, so focusGroup is not needed
+        ,
+        listIndex = focusedMasterListIndex,
+      )
+    // }
   }
 }
 
